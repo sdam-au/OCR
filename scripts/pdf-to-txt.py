ffe9282de06c1e5787ee3e8389c21f7b408862a2
kasev <vojtech.kase@gmail.com>
2020-04-28 10:19:57 +0200

    script clearing;

--- scripts/pdf-to-txt.py
+++ scripts/pdf-to-txt.py
@@ -1,33 +1,34 @@
-### binding for tesseract:
+# binding for tesseract:
 import pytesseract
-### computer vision:
+# computer vision:
 import cv2
-### computer vision relies to a substantial extent on numpy arrays
+# computer vision relies to a substantial extent on numpy arrays
 import numpy as np
-### PyMuPDF is called fitz:
+# PyMuPDF is called fitz:
 import fitz
-### to plot pages and everything else:
+# to plot pages and everything else:
 from datetime import datetime
-### configure sddk session
+# configure sddk session
 
-### CRUCIAL FUNCTIONS
+
+# CRUCIAL FUNCTIONS
 def pix2np(pix):
     im = np.frombuffer(pix.samples, dtype=np.uint8).reshape(pix.h, pix.w, pix.n)
     #im = np.ascontiguousarray(im[..., [2, 1, 0]])  # rgb to bgr
     return im
 
 def get_text(doc):
-        i = 1
-        pages = ""
-        for page in doc: ### or you can specify: doc(start, end, step):
-            pix = page.getPixmap(matrix = fitz.Matrix(2, 2), colorspace="csGRAY")  # try "csGRAY"
-            img = pix2np(pix)
-            kernel = np.ones((2, 2), np.uint8)
-            img_er = cv2.erode(img, kernel, iterations=1)
-            txt = pytesseract.image_to_string(img_er, lang=language) + "\n\n[end-of-page" + str(i) + "]\n\n"
-            pages += txt 
-            i = i+1
-        return pages
+    i = 1
+    pages = ""
+    for page in doc: # or you can specify: doc(start, end, step):
+        pix = page.getPixmap(matrix = fitz.Matrix(2, 2), colorspace="csGRAY")  # try "csGRAY"
+        img = pix2np(pix)
+        kernel = np.ones((2, 2), np.uint8)
+        img_er = cv2.erode(img, kernel, iterations=1)
+        txt = pytesseract.image_to_string(img_er, lang=language) + "\n\n[end-of-page" + str(i) + "]\n\n"
+        pages += txt 
+        i = i+1
+    return pages
 
 inputfile = input("file for ocr: ")
 try:
@@ -44,8 +45,7 @@ pages_str = get_text(doc)
 
 outputfile = input("specify name of the output file: ")
 
-### SAVE THE FILE
-file = open(outputfile,"w")
+# SAVE THE FILE
+file = open(outputfile, "w")
 file.write(pages_str)
-print(datetime.now(), "ocr analysis ended and the outpu file was saved.")
-
+print(datetime.now(), "ocr analysis ended and the output file was saved.")
